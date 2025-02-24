f9418dac9e6cc0ebf0d2a8a8e35c09549f0e1013
KINGALVI <alvialmahamud@gmail.com>
2025-02-25 00:35:50 +0200

    edited some code

--- src/Components/Bookmark/Bookmarks.jsx
+++ src/Components/Bookmark/Bookmarks.jsx
@@ -4,7 +4,7 @@ import BookmarksList from "./Bookmarks-List";
 const Bookmark = ({ Bookmark }) => {
     return (
         <>
-            <section className="md:w-1/3 p-5 bg-gray-300">
+            <section className="md:w-1/3 p-5 bg-gray-300 rounded-xl">
                 <h1 className="text-3xl text-center">Bookmarked Blogs : {Bookmark.length}</h1>
                 {
                     Bookmark.map(Bookmark => <BookmarksList key={Bookmark.id} Bookmark={Bookmark}></BookmarksList>)
