730961c74a1651f451a90697714d9d8cd941fdf0
KINGALVI <alvialmahamud@gmail.com>
2025-02-25 01:29:13 +0200

    edited some code

--- src/Components/Blogs/Blog-List.jsx
+++ src/Components/Blogs/Blog-List.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import PropTypes from 'prop-types';
 import BlogSection from "./Blog-Section";
 
-const BlogsList = ({ handelBooksmarks , handelMarkAsRead }) => {
+const BlogsList = ({ handelBooksmarks, handelMarkAsRead }) => {
 
     const [API, laodAPI] = useState([]);
 
